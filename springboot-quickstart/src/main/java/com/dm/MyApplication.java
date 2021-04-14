4b119f52f9419126b630318b9eb617e4a9352147
dm13579 <dongming053021@163.com>
2021-04-14 13:41:48 +0800

    init

---
+++ springboot-quickstart/src/main/java/com/dm/MyApplication.java
@@ -0,0 +1,29 @@
+package com.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  *                  ,;,,;
+  *                ,;;'(    
+  *      __      ,;;' ' \   
+  *   /'  '\'~~'~' \ /'\.)  
+  * ,;(      )    /  |.     
+  *,;' \    /-.,,(   ) \    
+  *     ) /       ) / )|    
+  *     ||        ||  \)     
+  *    (_\       (_\
+  *@className MyApplication
+  *@cescription 启动类
+  *@author dm
+  *@date 2021/4/14 13:37
+  *@slogan: 我自横刀向天笑，笑完我就去睡觉
+  *@version 1.0
+  **/
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
